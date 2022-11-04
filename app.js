dd1d2b605d7a5b4029c8618b9b2bf17809c788bb
Edet Asuquo <eddy1759@gmail.com>
2022-11-04 15:05:44 +0100

    update: updated the app file

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
-app.post("/calculate", arithOpts)
+app.post("/", arithOpts)
 
 // Error handler middleware
 app.use((error, req, res, next) => {
